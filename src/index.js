3441185687904d82db2bacb33212e61f7295c891
Ismarji Workman <ismarjiw@protonmail.com>
2024-02-05 15:45:23 -0500

    implement calculateTotal method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  if (tax < 0) {
+    tax = Math.abs(tax);
+  }
+
+  let total = 0
+
+  for (let item of items) {
+    if (item.taxable) {
+      total += item.price
+      const taxedItem = item.price * tax;
+      total += taxedItem
+    } else {
+      total += item.price
+    }
+  }
+
+  return total
 }
